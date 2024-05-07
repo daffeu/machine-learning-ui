1b61631cb075e7b8b6eea11ed59445a6ced07927
Pavel <egorovpavel331@mail.ru>
2024-05-07 13:04:17 +0300

    Update Apollo optimizer

--- src/mlui/CustomOptimizers/Apollo.py
+++ src/mlui/CustomOptimizers/Apollo.py
@@ -29,8 +29,6 @@ class Apollo(Optimizer):
         The initial learning rate for warmup.
     weight_decay : float, default: 0.0
         The weight decay coefficient.
-    weight_decay_type : str, default: “L2”
-        The type of weight decay:  “L2” | “decoupled” | “stable”.
     """
     def __init__(
             self,
@@ -41,7 +39,6 @@ class Apollo(Optimizer):
             warmup=500,
             rebound="constant",
             weight_decay=0.0,
-            weight_decay_type="L2",
             clipnorm=None,
             clipvalue=None,
             global_clipnorm=None,
@@ -64,8 +61,6 @@ class Apollo(Optimizer):
             **kwargs
         )
 
-        if rebound not in ['constant', 'belief']:
-            raise ValueError("Invalid recitifed bound: {}".format(rebound))
         if init_lr is None:
             init_lr=learning_rate/1000
 
@@ -76,14 +71,13 @@ class Apollo(Optimizer):
         self.init_lr = init_lr
         self.warmup = warmup
         self.rebound_type = rebound
-        self.weight_decay_type = weight_decay_type
 
     def build(self, var_list):
         """Initialize optimizer variables.
 
         Apollo optimizer has 3 types of variables: exponential moving average
-        of gradient values, exponential moving average of squared gradient
-        values and previous update direction.
+        of gradient values, hessian approximation values
+        and previous update direction.
 
         Parameters
         ----------
@@ -103,7 +97,7 @@ class Apollo(Optimizer):
         for var in var_list:
             self._exp_avg_grads.append(
                 self.add_variable_from_reference(
-                    model_variable=var, variable_name="exp_avg_grad"
+                    model_variable=var, variable_name="m"
                 )
             )
 
@@ -125,7 +119,6 @@ class Apollo(Optimizer):
 
         curr_lr = tf.cast(self.learning_rate, variable.dtype)
         weight_decay = tf.cast(self.weight_decay, variable.dtype)
-        wdt = self.weight_decay_type
         beta = tf.cast(self.beta, variable.dtype)
         eps = tf.cast(self.epsilon, variable.dtype)
         warmup = tf.cast(self.warmup, variable.dtype)
@@ -133,7 +126,7 @@ class Apollo(Optimizer):
         step = tf.cast(self.iterations + 1, variable.dtype)
 
         var_key = self._var_key(variable)
-        exp_avg_grad = self._exp_avg_grads[self._index_dict[var_key]]
+        m = self._exp_avg_grads[self._index_dict[var_key]]
         B = self._approx_hessians[self._index_dict[var_key]]
         d_p = self._updates[self._index_dict[var_key]]
 
@@ -144,44 +137,34 @@ class Apollo(Optimizer):
         bias_correction = 1 - beta ** step
         alpha = (1 - beta) / bias_correction
 
-        # Perform step weight decay
-        if weight_decay != 0 and wdt == "L2":
-            gradient = gradient + variable * weight_decay
-
-        # calc the diff grad
-        delta_grad = gradient - exp_avg_grad
+        delta_grad = gradient - m
         if self.rebound_type == "belief":
             rebound = tf.norm(delta_grad, ord=np.inf)
         else:
             rebound = 0.01
             eps = eps / rebound
 
-        # Update the running average grad
-        exp_avg_grad_t = exp_avg_grad.assign_add(delta_grad * alpha)
+        m.assign_add(delta_grad * alpha)
 
         denom = tf.norm(d_p, ord=4) + eps
-        d_p_t = d_p / denom
-        v_sq = d_p_t ** 2
-        delta = tf.math.reduce_sum((delta_grad / denom) * d_p_t) * (-alpha) - tf.math.reduce_sum(B * v_sq)
+        d_p_t = tf.divide(d_p, denom)
+        v_sq = tf.multiply(d_p_t, d_p_t)
+        delta = tf.math.reduce_sum(tf.multiply(tf.divide(d_p, denom), d_p_t)) *\
+                (-alpha) - tf.math.reduce_sum(tf.multiply(B, v_sq))
 
-        # Update B
-        B_t = B.assign_add(v_sq * delta)
+        B.assign_add(v_sq * delta)
 
-        # calc direction of parameter updates
         if self.rebound_type == 'belief':
-            denom = tf.math.maximum(tf.math.abs(B_t), rebound) + eps / alpha
+            denom = tf.math.maximum(tf.math.abs(B), rebound) + tf.divide(eps, alpha)
         else:
-            denom = tf.math.maximum(tf.math.abs(B_t), rebound)
+            denom = tf.math.maximum(tf.math.abs(B), rebound)
 
-        d_p_t = exp_avg_grad_t / denom
+        d_p_t = tf.divide(m, denom)
 
-        # Perform step weight decay (decoupled)
-        if weight_decay != 0 and wdt != "L2":
-            if wdt == "stable":
-                weight_decay_t = weight_decay / tf.reduce_mean(denom)
-            else:
-                weight_decay_t = weight_decay
-            d_p_t = d_p.assign(d_p_t + variable * weight_decay_t)
+        if weight_decay != 0:
+            d_p_t = d_p.assign(d_p_t + variable * weight_decay)
+        else:
+            d_p_t = d_p.assign(d_p_t)
 
         variable.assign_add(d_p_t * -curr_lr)
 
